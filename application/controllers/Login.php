e5fca4e4e3877f3e6a333d00793ac7fb0cdd4282
Emiliano Martinez Burgos <miloaltrock@gmail.com>
2019-12-15 17:33:51 -0400

    Modificado el login, ahora en vez de cambiar la vista desde el controlador se envia la url por json y se cambia en el archivo js Por ahora lleva a la vista de trabajadores independiente de los datos escritos

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -24,9 +24,11 @@ class Login extends CI_Controller{
 				'rut' => form_error('rut'),
 				'password' => form_error('password'));
 				echo json_encode($errors);
+				$this->output->set_status_header(400);
 			/*$data['menu'] = getmainmenu();
 			$this->load->view('page-login.php',$data);*/
 		}else{
+			echo json_encode(array('url' => base_url('Jefe_Turno/trabajadores')));
 			//if(rol === 'PLANIFICADOR'){
 
 			//}elseif (rol === 'JEFETURNO') {
--- assets/js/auth/login.js
+++ assets/js/auth/login.js
@@ -5,17 +5,20 @@
 			url:'login/validate',
 			type: 'POST',
 			data: $(this).serialize(),
-			success: function(data){
-				var json = JSON.parse(data);
-				if(Object.keys(json.rut).length){
-					alert(json.rut);
-				}
-				if(Object.keys(json.password).length){
-					alert(json.password);
-				}
+			success: function(wew){
+				var json = JSON.parse(wew);
+				window.location.replace(json.url);
 			},
-			error: function(){
-				alert("error");
+			error: function(xhr){
+				if(xhr.status == 400){
+					var json = JSON.parse(xhr.responseText);
+					if(Object.keys(json.rut).length){
+						alert(json.rut);
+					}
+					if(Object.keys(json.password).length){
+						alert(json.password);
+					}
+				}
 			},
 		});
 	});
