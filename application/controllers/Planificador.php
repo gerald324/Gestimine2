15bf8056c7707a352b1b1209b622fd23eceb21ad
Jamett <kyo.akamatsu@gmail.com>
2019-12-26 16:43:17 -0300

    Cambios en archivo plan minero

--- application/controllers/Planificador.php
+++ application/controllers/Planificador.php
@@ -8,7 +8,7 @@ class Planificador extends CI_Controller{
 		$content_data = array(
 			'usuarios' => $datos['Usuarios']
 		);
-		$this->load->view('jefe_turno_inicio.php');
+		$this->load->view('planificador_inicio.php');
 	}
 
 	public function plan_minero(){
@@ -29,4 +29,13 @@ class Planificador extends CI_Controller{
 		$this->load->view('planificador_alerta.php');
 	}
 
+	public function editar(){
+		//cambiar modelo por modelo del plan minero
+		$this->load->model('Usuarios_model', 'UM', true);
+		$datos['Usuarios']=$this->UM->getAll();
+		$content_data = array(
+
+		);
+		$this->load->view('editar_plan.php');
+	}
 }
