8b149dc832238afeb00d76727a21a7dc8ac1d98b
Jamett <kyo.akamatsu@gmail.com>
2020-01-15 01:27:10 -0300

    agregadas vistas restantes

--- application/controllers/Planificador.php
+++ application/controllers/Planificador.php
@@ -29,6 +29,42 @@ class Planificador extends CI_Controller{
 		$this->load->view('planificador_plan_carguio.php');
 	}
 
+	public function cronograma(){
+		$this->load->model('Usuarios_model','UM',true);
+		$datos['Usuarios']=$this->UM->getAll();
+		$content_data = array(
+			'usuarios' => $datos['Usuarios']
+		);
+		$this->load->view('planificador_plan_cronograma.php');
+	}
+
+	public function perforacion(){
+		$this->load->model('Usuarios_model','UM',true);
+		$datos['Usuarios']=$this->UM->getAll();
+		$content_data = array(
+			'usuarios' => $datos['Usuarios']
+		);
+		$this->load->view('planificador_plan_cronograma.php');
+	}
+
+	public function transporte(){
+		$this->load->model('Usuarios_model','UM',true);
+		$datos['Usuarios']=$this->UM->getAll();
+		$content_data = array(
+			'usuarios' => $datos['Usuarios']
+		);
+		$this->load->view('planificador_plan_transporte.php');
+	}
+
+	public function tronadura(){
+		$this->load->model('Usuarios_model','UM',true);
+		$datos['Usuarios']=$this->UM->getAll();
+		$content_data = array(
+			'usuarios' => $datos['Usuarios']
+		);
+		$this->load->view('planificador_plan_tronadura.php');
+	}
+
 	public function alertas(){
 		$this->load->model('Usuarios_model','UM',true);
 		$datos['Usuarios']=$this->UM->getAll();
